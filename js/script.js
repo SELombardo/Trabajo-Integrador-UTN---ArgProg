d63b54e8e1d148fd6bc4769640e470065eda3e0b
Santiago Lombardo <lombardo.s.e@gmail.com>
2023-03-30 17:32:43 -0300

    versión inicial seccion comentarios

---
+++ js/script.js
@@ -0,0 +1,29 @@
+const comentArr = []
+
+const comentForm = document.querySelector(".comentForm")
+const cardContainer = document.querySelector(".comentContainer")
+
+const renderComent = () =>{
+    cardContainer.innerHTML = ""
+    for(const opinion of comentArr){
+        cardContainer.innerHTML += `
+        <div class="cardOpinion">
+            <img class="cardImg" src="https://static.vecteezy.com/system/resources/previews/007/226/475/non_2x/user-account-circle-glyph-color-icon-user-profile-picture-userpic-silhouette-symbol-on-white-background-with-no-outline-negative-space-illustration-vector.jpg">
+            <div>
+                <h3>${opinion.nombre}:</h3>
+                <p>"${opinion.coment}"</p>
+            </div>
+        </div>
+        `
+    }
+}
+
+comentForm.addEventListener("submit", (event) =>{
+    event.preventDefault()
+    comentArr.push({
+        nombre: comentForm.nombre.value, 
+        coment: comentForm.coment.value,
+    })
+    comentForm.reset()
+    renderComent()
+})
