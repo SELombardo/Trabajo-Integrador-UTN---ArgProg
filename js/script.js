e2124168a254ec787f5b9fba8febcc8cfb4763d3
ivan-pardo <yvanpardoalcocer@gmail.com>
2023-04-01 19:57:30 -0300

    seccion preguntas-frecuentes

--- js/script.js
+++ js/script.js
@@ -28,3 +28,35 @@ comentForm.addEventListener("submit", (event) =>{
     comentForm.reset()
     renderComent()
 })
+
+
+// SCRIPT PREGUNTAS FRECUENTES 
+let pregunta = document.querySelectorAll('.pregunta-frecuente');
+let botonDer = document.querySelectorAll('.simb-der');
+let respuesta = document.querySelectorAll('.respuesta-frecuente');
+let parrafo = document.querySelectorAll('.respuesta-frecuente p');
+
+for (let i = 0; i < botonDer.length; i = i + 1) {
+    let altoparrafo = parrafo[i].clientHeight;
+    let conttador = 0;
+
+    botonDer[i].addEventListener('click', () => {
+        if ( conttador == 0 ) {
+            respuesta[i].style.height = `${altoparrafo}px`;
+            pregunta[i].style.marginBottom = '10px';
+            botonDer[i].innerHTML = '<i>-</i>';
+            conttador = conttador + 1
+        }
+
+        else if ( conttador == 1 ) {
+            respuesta[i].style.height = `0`;
+            pregunta[i].style.marginBottom = '0';
+            botonDer[i].innerHTML = '<i class="bi bi-chevron-compact-down"></i>';
+            conttador = conttador - 1
+        }
+    })
+}
+
+console.log(parrafo)
+
+// --------------------------------
